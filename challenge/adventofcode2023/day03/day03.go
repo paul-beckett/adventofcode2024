e8b8977e6fd088323bfcd58d63359f14632bb2ea
paul-beckett <paul.beckett@bjss.com>
2024-11-23 20:58:42 +0000

    2023 day03 solution

--- challenge/adventofcode2023/day03/day03.go
+++ challenge/adventofcode2023/day03/day03.go
@@ -1,6 +1,11 @@
 package day03
 
-import "regexp"
+import (
+	"adventofcode2024/util/map_reduce"
+	"regexp"
+	"strconv"
+	"unicode"
+)
 
 type Day03 struct {
 	data []string
@@ -12,13 +17,59 @@ func newDay03(data []string) *Day03 {
 
 func (d *Day03) part1() int {
 	re := regexp.MustCompile("[0-9]+")
-	var rowMatches [][][]int
-	for _, text := range d.data {
-		rowMatches = append(rowMatches, re.FindAllStringSubmatchIndex(text, -1))
+	var parts []int
+	for row, text := range d.data {
+		rowMatches := re.FindAllStringSubmatchIndex(text, -1)
+		for _, match := range rowMatches {
+			if isPart(match[0], match[1], row, d.data) {
+				n, _ := strconv.Atoi(text[match[0]:match[1]])
+				parts = append(parts, n)
+			}
+		}
 	}
-	return -1
+	return map_reduce.Sum(parts)
+}
+
+func isPart(start, end int, row int, data []string) bool {
+	for offset := max(row-1, 0); offset <= min(row+1, len(data)-1); offset++ {
+		for i := max(start-1, 0); i < min(end+1, len(data[offset])-1); i++ {
+			r := rune(data[offset][i])
+			if !unicode.IsDigit(r) && r != '.' {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (d *Day03) part2() int {
-	panic("not implemented")
+	var gears [][]int
+	for row, text := range d.data {
+		for col, c := range text {
+			if c == '*' {
+				gears = append(gears, findNumbers(row, col, d.data))
+			}
+		}
+	}
+	gears = map_reduce.Filter(gears, func(gear []int) bool {
+		return len(gear) == 2
+	})
+	return map_reduce.Reduce(gears, func(sum int, gear []int) int {
+		return sum + gear[0]*gear[1]
+	}, 0)
+}
+
+func findNumbers(row, col int, data []string) []int {
+	re := regexp.MustCompile("[0-9]+")
+	var numbers []int
+	for offset := max(row-1, 0); offset <= min(row+1, len(data)-1); offset++ {
+		numberRanges := re.FindAllStringSubmatchIndex(data[offset], -1)
+		for _, number := range numberRanges {
+			if col >= number[0]-1 && col <= number[1] {
+				n, _ := strconv.Atoi(data[offset][number[0]:number[1]])
+				numbers = append(numbers, n)
+			}
+		}
+	}
+	return numbers
 }
